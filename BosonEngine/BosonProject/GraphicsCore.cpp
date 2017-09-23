685c7e1b6a8418b0fb2128756c0973d0233dfb46
Sean Connolly (RIT Student) <spc1802@ad.rit.edu>
2017-09-22 23:55:12 -0400

    Worked on Graphics core.
    Need to do more to get it rendering.

--- BosonEngine/BosonProject/GraphicsCore.cpp
+++ BosonEngine/BosonProject/GraphicsCore.cpp
@@ -2,11 +2,53 @@
 
 
 
-GraphicsCore::GraphicsCore()
+GraphicsCore::GraphicsCore(unsigned int width, unsigned int height, HWND hWnd)
+	: width(width), height(height), hWnd(hWnd)
 {
+	device = nullptr;
+	context = nullptr;
+	swapChain = nullptr;
+	backBuffer = nullptr;
+	depthStencilView = nullptr;
 }
 
 
 GraphicsCore::~GraphicsCore()
 {
+	if (depthStencilView) { depthStencilView->Release(); }
+	if (backBuffer) { backBuffer->Release(); }
+
+	if (swapChain) { swapChain->Release(); }
+	if (context) { context->Release(); }
+	if (device) { device->Release(); }
+}
+
+HRESULT GraphicsCore::InitGraphics()
+{
+	unsigned int deviceFlags = 0;
+
+#if defined(DEBUG) | defined(_DEBUG)
+	deviceFlags |= D3D11_CREATE_DEVICE_DEBUG;
+#endif
+
+	DXGI_SWAP_CHAIN_DESC swapChainDesc;
+	ZeroMemory(&swapChainDesc, sizeof(DXGI_SWAP_CHAIN_DESC));
+
+	swapChainDesc.BufferCount = 1;
+	swapChainDesc.BufferDesc.Width = width;
+	swapChainDesc.BufferDesc.Height = height;
+	swapChainDesc.BufferDesc.RefreshRate.Numerator = 60;
+	swapChainDesc.BufferDesc.RefreshRate.Denominator = 1;
+	swapChainDesc.BufferDesc.Format = DXGI_FORMAT_R8G8B8A8_UNORM;
+	swapChainDesc.BufferDesc.ScanlineOrdering = DXGI_MODE_SCANLINE_ORDER_UNSPECIFIED;
+	swapChainDesc.BufferDesc.Scaling = DXGI_MODE_SCALING_UNSPECIFIED;
+	swapChainDesc.BufferUsage = DXGI_USAGE_RENDER_TARGET_OUTPUT;
+	swapChainDesc.Flags = 0;
+	swapChainDesc.OutputWindow = hWnd;
+	swapChainDesc.SampleDesc.Count = 1;
+	swapChainDesc.SampleDesc.Quality = 0;
+	swapChainDesc.SwapEffect = DXGI_SWAP_EFFECT_DISCARD;
+	swapChainDesc.Windowed = true;
+
+	return E_NOTIMPL;
 }
--- BosonEngine/BosonProject/GraphicsCore.h
+++ BosonEngine/BosonProject/GraphicsCore.h
@@ -1,8 +1,28 @@
 #pragma once
+#include <d3d11.h>
+#include <Windows.h>
+
+//should this be a friend class of WindowsManager????
+
 class GraphicsCore
 {
 public:
-	GraphicsCore();
+	GraphicsCore(unsigned int width, unsigned int height, HWND hWnd);
 	~GraphicsCore();
+
+	HRESULT InitGraphics();
+
+private:
+	unsigned int width;
+	unsigned int height;
+	HWND hWnd;
+
+	D3D_FEATURE_LEVEL featureLvl;
+	IDXGISwapChain* swapChain;
+	ID3D11Device* device;
+	ID3D11DeviceContext* context;
+
+	ID3D11RenderTargetView* backBuffer;
+	ID3D11DepthStencilView* depthStencilView;
 };
 
